e37f982094b4a658afc0ea953b02967a8c3c14d1
Ben Plessinger <ben@plessinger.us>
2015-03-03 00:12:06 -0500

    Initial Commit

---
+++ client/js/app.js
@@ -0,0 +1,71 @@
+var app = angular.module("ngEcho", ["btford.socket-io", "mgcrea.ngStrap", "smart-table", "angularMoment"]);
+
+app.factory("socket", function (socketFactory) {
+	return socketFactory();
+});
+
+app.factory("echoService", ["$http", "socket", function ($http, socket) {
+	return {
+		sendHttpMessage: function (message) {
+			return $http.post("/echo/", {
+				message: message
+			});
+		},
+		webSocketEmit: function (message) {
+			socket.emit("echo", {
+				message: message
+			});
+		},
+		sendHttpMessageRecieveSocketResponse: function (message) {
+			return $http.post("/echo/?emit", {
+				message: message
+			});
+		}
+	};
+}]);
+
+app.controller("echoCtrl", ["socket", "echoService", function echoCtrl(socket, echoService) {
+	var self = this;
+	self.rowCollection = [];
+	self.debugSocket = localStorage.debug === "" ? false : true;
+	self.socket = socket;
+	self.message = "";
+	self.socket.on("echo", function echo(data) {
+		var row = {
+			timestamp: moment(),
+			type: "socket",
+			message: data.message
+		};
+		self.rowCollection.push(row);
+	});
+	self.toggleDebug = function toggleDebug() {
+		if (localStorage.debug === "*") {
+			localStorage.debug = "";
+			self.debugSocket = false;
+		}
+		else {
+			localStorage.debug = "*";
+			self.debugSocket = true;
+		}
+	};
+	self.sendHttpMessage = function sendHttpMessage() {
+		echoService.sendHttpMessage(self.message)
+			.success(function (data) {
+				var row = {
+					timestamp: new Date(),
+					type: "http",
+					message: data.message
+				};
+				self.rowCollection.push(row);
+				console.log(this, data);
+			});
+	};
+	self.webSocketEmit = function webSocketEmit() {
+		echoService.webSocketEmit(self.message);
+	};
+	self.sendHttpMessageRecieveSocketResponse = function sendHttpMessageRecieveSocketResponse() {
+		echoService.sendHttpMessageRecieveSocketResponse(self.message).then(function (data, status, headers, config) {
+			console.log(data, status, headers, config);
+		});
+	};
+}]);
---
+++ server/app.js
@@ -0,0 +1,49 @@
+var express = require("express"),
+	path = require("path"),
+	app = express(),
+	server = require("http").Server(app),
+	bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.io = require("socket.io")(server);
+
+server.listen(8000, function serverListen() {
+	console.log("server running on", this._connectionKey);
+});
+
+app.use("/bower_components", express.static(path.join(__dirname, "..", "client", "bower_components")));
+app.use("/js", express.static(path.join(__dirname, "..", "client", "js")));
+
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/echo/", function (req, res) {
+	if (typeof req.query.emit !== "undefined") {
+		res.sendStatus(202);
+
+		setTimeout(function () {
+			app.io.emit("echo", req.body);
+		}, req.query.wait || 1000);
+
+	}
+	else {
+		res.json(req.body);
+	}
+
+});
+
+app.io.on("connection", function (socket) {
+	socket.emit("echo", {
+		message: "Connected"
+	});
+	socket.on("echo", function (data) {
+		socket.emit("echo", data);
+	});
+});
